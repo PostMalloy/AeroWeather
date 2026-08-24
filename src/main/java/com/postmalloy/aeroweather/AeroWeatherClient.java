f0a4e5787b33e04201966ebec8f98797f475911d
PostMalloy <maxgriswold77@gmail.com>
2026-08-24 10:17:40 -0400

    Fix client crash: drop stale @EventBusSubscriber from AeroWeatherClient
    Stripping the stock onClientSetup() method (M0) left the class-level
    @EventBusSubscriber annotation behind with no @SubscribeEvent methods
    left to register, which FML treats as fatal at mod-construction time:
    "class AeroWeatherClient has no @SubscribeEvent methods, but register
    was called anyway." The class still needs to exist for the config
    screen registration in its constructor, just not the annotation.
    
    Verified: runClient now proceeds past mod loading into normal client
    startup (texture atlases, sound engine, etc.) instead of crashing.

--- src/main/java/com/postmalloy/aeroweather/AeroWeatherClient.java
+++ src/main/java/com/postmalloy/aeroweather/AeroWeatherClient.java
@@ -2,15 +2,12 @@ package com.postmalloy.aeroweather;
 
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModContainer;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
 @Mod(value = AeroWeather.MODID, dist = Dist.CLIENT)
-// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = AeroWeather.MODID, value = Dist.CLIENT)
 public class AeroWeatherClient {
     public AeroWeatherClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
